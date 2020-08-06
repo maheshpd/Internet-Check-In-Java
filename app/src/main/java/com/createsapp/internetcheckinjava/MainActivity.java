c9499f0244109aa7b5a7e64081dfe85cb89ef295
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-07 02:43:03 +0530

    initial commit2

--- app/src/main/java/com/createsapp/internetcheckinjava/MainActivity.java
+++ app/src/main/java/com/createsapp/internetcheckinjava/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Check Internt Connection
+
         ConnectionLiveData connectionLiveData = new ConnectionLiveData(getApplicationContext());
         connectionLiveData.observe(this, new Observer<ConnectionModel>() {
             @Override
